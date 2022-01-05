53ced7ba8e86ac926e6976eb3ed79a9e22d74b93
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-01-05 15:56:19 -0600

    added delete task functionality

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -1,3 +1,18 @@
+var pageContentEl = document.querySelector("#page-content");
+var taskButtonHandler = function(event) {
+    console.log(event.target);
+    if (event.target.matches(".delete-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+pageContentEl.addEventListener("click", taskButtonHandler);
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+};
+
 var taskIdCounter = 0;
 
 var formEl = document.querySelector("#task-form");
